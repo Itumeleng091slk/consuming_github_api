755a5315298e1d3eb661dad24964ac8ce57a3cf1
Itumeleng091slk <magdalene.selokela@umuzi.org>
2020-03-25 09:32:22 +0200

    second

--- dates.py
+++ dates.py
@@ -1,16 +1,42 @@
+from github import Github
+import urllib3
+import requests
+import json
 from datetime import datetime
 from datetime import timedelta, date
-import api
+from api import repos
+
+from secrets import username
+from secrets import password
+from secrets import token
+
+g = Github("username","password")
+repo = g.get_repo('repo')
+date = repo.updated_at
+date = int(2017-5-2)
+print(date)
 
 
-def daterange(start_date, end_date):
-    for n in range(int((end_date - start_date).days)):
-        yield start_date + timedelta(n)
+
+# for pull in r.get_pulls('all'):
+#     print(get_pull)
+# parameters = {"sort": 'pushed'}
+# r = requests.get("https://github.com/Itumeleng091slk/Mock_and_Spies.git",params=parameters)
+# len(r.json()) == 13
+# for item in resp['updated_at']:
+#     print(item['updated_at'])
+# def daterange(start_date, end_date):
+#     for n in range(int((end_date - start_date).days)):
+#         yield start_date + timedelta(n)
 
 
-start_date = date(2019, 12, 1)
-end_date = date(2020, 3, 5)
+# start_date = date(2020, 1, 12)
+# end_date = date(2020, 3, 5)
 
-for single_date in daterange(start_date,end_date):
-    date_obj = datetime.combine(single_date,datetime.min.time())
-    print(single_date.strftime('%Y-%m-%d:'))
+# for single_date in daterange(start_date,end_date):
+#     date_obj = datetime.combine(single_date,datetime.min.time())
+#     print(single_date.strftime('%Y-%m-%d:'), 'repos')
+
+# def get_author(author, repo,start_date,end_date):
+#     author_prs = "username:{}/{} type:pr author:{} is merged:{}..{}" .format(repo,start_date,end_start)
+#     return author_prs
