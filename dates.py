912bf297fecc60a481bde70cf9fd4a06b58edb87
Itumeleng091slk <magdalene.selokela@umuzi.org>
2020-04-18 21:17:04 +0200

    update

--- dates.py
+++ dates.py
@@ -1,42 +1,30 @@
-from github import Github
-import urllib3
 import requests
-import json
-from datetime import datetime
-from datetime import timedelta, date
-from api import repos
+from datetime import date
+# pull_requests = requests.get("https://api.github.com/repos/Umuzi-org/tech-department/pulls?id=1")
 
-from secrets import username
-from secrets import password
-from secrets import token
+# print(pull_requests.json())
 
-g = Github("username","password")
-repo = g.get_repo('repo')
-date = repo.updated_at
-date = int(2017-5-2)
-print(date)
+def pull_requests(owner, repo_name, start_date, end_date):
 
 
+  pull_requests = requests.get(f"https://api.github.com/repos/{owner}/{repo_name}/pulls?id=1")
 
-# for pull in r.get_pulls('all'):
-#     print(get_pull)
-# parameters = {"sort": 'pushed'}
-# r = requests.get("https://github.com/Itumeleng091slk/Mock_and_Spies.git",params=parameters)
-# len(r.json()) == 13
-# for item in resp['updated_at']:
-#     print(item['updated_at'])
-# def daterange(start_date, end_date):
-#     for n in range(int((end_date - start_date).days)):
-#         yield start_date + timedelta(n)
+  return pull_requests
 
+print(dir(pull_requests("Umuzi-org","tech-department",18-3-2020,18-4-2020)))
 
-# start_date = date(2020, 1, 12)
-# end_date = date(2020, 3, 5)
+x = {
+  "id" : 1,
+  'created_at':2019-2-4,
+  'updated_at': 2020-4-27,
+  'pushed_at': 2020-4-21,
 
-# for single_date in daterange(start_date,end_date):
-#     date_obj = datetime.combine(single_date,datetime.min.time())
-#     print(single_date.strftime('%Y-%m-%d:'), 'repos')
+}
 
-# def get_author(author, repo,start_date,end_date):
-#     author_prs = "username:{}/{} type:pr author:{} is merged:{}..{}" .format(repo,start_date,end_start)
-#     return author_prs
+# for key, value in x.items():
+#   if value < 2018:
+#     print(value)
+f_date = date(2020, 3, 18)
+l_date = date(2020, 4, 18)
+pull_requests = l_date - f_date
+print(pull_requests.days)
