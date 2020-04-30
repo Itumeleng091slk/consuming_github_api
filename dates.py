2cc5f476ac87bb326e565b3fd9e3c975343270ac
Itumeleng091slk <59916899+Itumeleng091slk@users.noreply.github.com>
2020-05-01 00:14:00 +0200

    Update dates.py

--- dates.py
+++ dates.py
@@ -1,27 +1,26 @@
-import requests
-from datetime import date
+import github
 
-pull_requests = requests.get("https://api.github.com/repos/Umuzi-org/tech-department/pulls?id=1")
+pull_requests = requests.get("https://api.github.com/repos/Umuzi-org/tech-department/pulls?id=434")
+
 print(pull_requests.json())
 
 def pull_requests(owner, repo_name, start_date, end_date):
-  pull_requests = requests.get(f"https://api.github.com/repos/{owner}/{repo_name}/pulls?id=1")
+
+  pull_requests = requests.get(f"https://api.github.com/repos/{owner}/{repo_name}/pulls?id=434")
+
   return pull_requests
 
-print(dir(pull_requests("Umuzi-org","tech-department",18-3-2020,18-4-2020)))
+print(dir(pull_requests("Umuzi-org","tech-department",18-3-2019,18-4-2020)))
 
-# x = {
-#   "id" : 1,
-#   'created_at':2019-2-4,
-#   'updated_at': 2020-4-27,
-#   'pushed_at': 2020-4-21,
+x = {
+  'created_at': 2018,
+  'updated_at': 2020,
+  'pushed_at': 2020,
+  'closed_at': 2021 
 
-# }
 
-# # for key, value in x.items():
-# #   if value < 2018:
-# #     print(value)
-# f_date = date(2020, 3, 18)
-# l_date = date(2020, 4, 18)
-# pull_requests = l_date - f_date
-# print(pull_requests.days)
+ }
+
+for key, value in x.items():
+  if value < 2020 :
+    print(value)
