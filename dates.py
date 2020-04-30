3826f7b1122a5f8064a9ca6ee7821be423467e70
Itumeleng091slk <59916899+Itumeleng091slk@users.noreply.github.com>
2020-05-01 00:19:32 +0200

    Update dates.py

--- dates.py
+++ dates.py
@@ -1,16 +1,14 @@
 import github
 
 pull_requests = requests.get("https://api.github.com/repos/Umuzi-org/tech-department/pulls?id=434")
-
 print(pull_requests.json())
 
 def pull_requests(owner, repo_name, start_date, end_date):
 
   pull_requests = requests.get(f"https://api.github.com/repos/{owner}/{repo_name}/pulls?id=434")
-
   return pull_requests
 
-print(dir(pull_requests("Umuzi-org","tech-department",18-3-2019,18-4-2020)))
+print(dir(pull_requests("Umuzi-org","tech-department",18-3-2018,18-4-2020)))
 
 x = {
   'created_at': 2018,
