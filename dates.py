384c0f24e2561027a77fee7e2ae8e5d3229ea81e
Itumeleng091slk <59916899+Itumeleng091slk@users.noreply.github.com>
2020-04-30 23:53:06 +0200

    Update dates.py

--- dates.py
+++ dates.py
@@ -2,7 +2,6 @@ import requests
 from datetime import date
 
 pull_requests = requests.get("https://api.github.com/repos/Umuzi-org/tech-department/pulls?id=1")
-
 print(pull_requests.json())
 
 def pull_requests(owner, repo_name, start_date, end_date):
@@ -11,18 +10,18 @@ def pull_requests(owner, repo_name, start_date, end_date):
 
 print(dir(pull_requests("Umuzi-org","tech-department",18-3-2020,18-4-2020)))
 
-x = {
-  "id" : 1,
-  'created_at':2019-2-4,
-  'updated_at': 2020-4-27,
-  'pushed_at': 2020-4-21,
+# x = {
+#   "id" : 1,
+#   'created_at':2019-2-4,
+#   'updated_at': 2020-4-27,
+#   'pushed_at': 2020-4-21,
 
-}
+# }
 
-# for key, value in x.items():
-#   if value < 2018:
-#     print(value)
-f_date = date(2020, 3, 18)
-l_date = date(2020, 4, 18)
-pull_requests = l_date - f_date
-print(pull_requests.days)
+# # for key, value in x.items():
+# #   if value < 2018:
+# #     print(value)
+# f_date = date(2020, 3, 18)
+# l_date = date(2020, 4, 18)
+# pull_requests = l_date - f_date
+# print(pull_requests.days)
